ec53d4b195891887eadf92cef5be5e5ea8925bfa
Sitong <nickren93@gmail.com>
2025-03-17 15:50:55 -0400

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Nick</h1>
+      <img src="https://scontent-lga3-2.cdninstagram.com/v/t51.29350-15/471474745_1109257103907727_8622063062457760210_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDc0eDEzMTQuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-lga3-2.cdninstagram.com&_nc_cat=107&_nc_oc=Q6cZ2AFGvFYNr-uPSaTh4fww_a1Ic8hZxEOrZ5wZyHeNmxkaVmyN4_PiIRvBeFrKxXgQ2nE&_nc_ohc=y-6I74hCRyYQ7kNvgGl5per&_nc_gid=HK-5qxIx4dYVFxV7pzBv5A&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYFzDgxez5-abJxP7KX-5Py2-6Dzufo2r04hp9DQoxqEyw&oe=67DE57AE&_nc_sid=d885a2" 
+      alt="my picture" />
+      <h2>About Me</h2>
+      <p>
+        I am a software engineer graduating from Flatiron School
+      </p>
+      <a
+        href="https://github.com/nickren93"
+      >
+        GitHub
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nick-ren-7151b769/"
+      >
+        LinkedIn
+      </a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,70 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />)
+
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+    
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument(); 
+});
+
+test("displays an image of yourself with alt text identifying the content of the image", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const img = screen.getByRole("img", { name: 'my picture'})
+    // Assert
+    expect(img).toBeInTheDocument();
+});
+
+test("displays A second-level heading with the text `About Me`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /About Me/i,
+        exact: false,
+        level: 2,
+    });
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+});
+
+test("displays a paragraph for your biography", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const bio = screen.getByText(/I am a software engineer/i);
+    // Assert
+    expect(bio).toBeInTheDocument();
+});
+
+test("displays a link to your GitHub page", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const gitHubLink = screen.getByRole("link", {
+        name: /GitHub/i
+    });
+    // Assert
+    expect(gitHubLink).toBeInTheDocument();
+});
+
+test("displays a link to your LinkedIn page", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const linkedInLink = screen.getByRole("link", {
+        name: /LinkedIn/i
+    });
+    // Assert
+    expect(linkedInLink).toBeInTheDocument();
+});
